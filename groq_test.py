260ddf152e7d5dfdc32c3406bf8ba593be9e2d00
SeerPro <458199-SeerPro@users.noreply.replit.com>
2025-02-24 21:13:36 +0000

    User checkpoint: Remove obsolete gang data from backups

--- groq_test.py
+++ groq_test.py
@@ -5,26 +5,59 @@ from pydantic import BaseModel
 from groq import Groq
 import os
 groq_api_key = os.environ['GROQ_API_KEY']
-from rich import print
 
 groq = Groq(api_key=os.environ['GROQ_API_KEY'], base_url=os.environ['GROQ_BASE_URL'])
-import requests
-import os
 
-headers = {
-    "Authorization": f"Bearer {os.environ['GROQ_API_KEY']}",
-    "Content-Type": "application/json",
-}
 
-payload = json.dumps({
-    "messages": [
-        {
-            "role": "user",
-            "content": "Hello, I want to create a new gang to use in Necromunda for Escher with a starting point value of 1000 points.  Can you help?"
-        }
-    ],
-    "model": "llama-3.3-70b-versatile"
-})
+# Data model for LLM to generate
+class Ingredient(BaseModel):
+    name: str
+    quantity: str
+    quantity_unit: Optional[str]
 
-req = requests.post(url="https://api.groq.com/openai/v1/chat/completions", headers=headers, data=payload)
-print(req.text)
+
+class Recipe(BaseModel):
+    recipe_name: str
+    ingredients: List[Ingredient]
+    directions: List[str]
+
+
+def get_recipe(recipe_name: str) -> Recipe:
+    chat_completion = groq.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a recipe database that outputs recipes in JSON.\n"
+                # Pass the json schema to the model. Pretty printing improves results.
+                f" The JSON object must use the schema: {json.dumps(Recipe.model_json_schema(), indent=2)}",
+            },
+            {
+                "role": "user",
+                "content": f"Fetch a recipe for {recipe_name}",
+            },
+        ],
+        model="llama-3.3-70b-versatile",
+        temperature=0,
+        # Streaming is not supported in JSON mode
+        stream=False,
+        # Enable JSON mode by setting the response format
+        response_format={"type": "json_object"},
+    )
+    return Recipe.model_validate_json(chat_completion.choices[0].message.content)
+
+
+def print_recipe(recipe: Recipe):
+    print("Recipe:", recipe.recipe_name)
+
+    print("\nIngredients:")
+    for ingredient in recipe.ingredients:
+        print(
+            f"- {ingredient.name}: {ingredient.quantity} {ingredient.quantity_unit or ''}"
+        )
+    print("\nDirections:")
+    for step, direction in enumerate(recipe.directions, start=1):
+        print(f"{step}. {direction}")
+
+
+recipe = get_recipe("apple pie")
+print_recipe(recipe)
